b2cae098f629cb271d81ab358d12b30fe32b518b
naryu33 <145438564+naryu33@users.noreply.github.com>
2025-01-11 14:50:54 +0900

    first commit

---
+++ utils/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZB_ZhOZCYB0eubTmW47IUWf-3F5ajjzM",
+    authDomain: "yasuulab.firebaseapp.com",
+    projectId: "yasuulab",
+    storageBucket: "yasuulab.firebasestorage.app",
+    messagingSenderId: "210584026290",
+    appId: "1:210584026290:web:be1e9237bde8a3c8ecddab",
+    measurementId: "G-200KM2GHCH"
+};
+
+// Firebaseの初期化
+firebase.initializeApp(firebaseConfig);
